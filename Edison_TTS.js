ab792457beadd5b99dc83e939a77cdb3b65b10a3
Hui Wah Chiang <hwwc518@gmail.com>
2017-04-09 02:57:47 -0400

    Initial commit
    Used IBM watson's text to speech to on web server data taken from an
    Arduino/ Intel Edison kit for walking stick users

---
+++ Edison_TTS.js
@@ -0,0 +1,16 @@
+var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var fs = require('fs');
+
+var text_to_speech = new TextToSpeechV1({
+  username: '8f48ee97-95f8-4017-bbf6-f21eb6283fa4',
+  password: 'otOagxxR4xHb'
+});
+
+var params = {
+  text: 'Hello from IBM Watson',
+  voice: 'en-US_AllisonVoice', // Optional voice
+  accept: 'audio/wav'
+};
+
+// Pipe the synthesized text to a file
+text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
