3ecf1f105720c5e70e17a75e64502b61dcf20fe9
Hui Wah Chiang <hwwc518@gmail.com>
2017-04-09 04:20:05 -0400

    Changes to play wav in node

--- Edison_TTS.js
+++ Edison_TTS.js
@@ -13,5 +13,24 @@ var params = {
 };
 
 // Pipe the synthesized text to a file
-var audio = text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
-audio.play;
+text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
+
+var play = require('play')//.Play();
+play.sound('output.wav');
+// play with a callback
+// play.sound('./wavs/sx/intro.wav', function(){
+//
+//   // these are all "fire and forget", no callback
+//   play.sound('./wavs/sfx/alarm.wav');
+//   play.sound('./wavs/sfx/crinkle.wav');
+//   play.sound('./wavs/sfx/flush.wav');
+//   play.sound('./wavs/sfx/ding.wav');
+//
+// });
+
+//If you want to know if this can't play for some reason
+// play.on('error', function () {
+//   console.log('I can't play!');
+// });
+//var audio = new Audio('output.wav');
+//audio.play();
